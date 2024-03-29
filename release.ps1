bf185b93601654ce21464be5839940e15909956a
shane <sgrimes@gmail.com>
2024-03-28 23:53:12 -0700

    oops. fixed rename-item

--- release.ps1
+++ release.ps1
@@ -7,7 +7,7 @@ New-Item -ItemType "directory" -Path ".\EDx52Display"
 
 go build
 
-Rename-Item -Path EDx52display_Redux.exe -NewName EDx52display.exe
-Copy-Item -Path EDx52display.exe,conf.yaml,LICENSE,README.md,names,DepInclude -Destination .\EDx52Display -Recurse
+Copy-Item -Path EDx52display_Redux.exe,conf.yaml,LICENSE,README.md,names,DepInclude -Destination .\EDx52Display -Recurse
+Rename-Item -Path .\EDx52Display\EDx52display_Redux.exe -NewName EDx52display.exe
 
 Compress-Archive .\EDx52Display\* Release.zip

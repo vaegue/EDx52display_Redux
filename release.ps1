0dd20aa99330f0928023c584e21d6d0dea14459c
shane <sgrimes@gmail.com>
2024-03-28 23:28:29 -0700

    less cmd more powershell. rename exe without _redux for ease of use

--- release.ps1
+++ release.ps1
@@ -3,10 +3,11 @@ go clean
 Remove-Item  .\EDx52Display -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item .\Release.zip -ErrorAction SilentlyContinue
 
-mkdir EDx52Display
+New-Item -ItemType "directory" -Path ".\EDx52Display"
 
 go build
 
+Rename-Item -Path EDx52display_Redux.exe -NewName EDx52display.exe
 Copy-Item -Path EDx52display.exe,conf.yaml,LICENSE,README.md,names,DepInclude -Destination .\EDx52Display -Recurse
 
 Compress-Archive .\EDx52Display\* Release.zip
